337e5158d0042093c833cd0ef9211d1b18f4c06c
Hari <152277436+HariharansudhanM@users.noreply.github.com>
2024-06-05 15:18:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ btn.addEventListener("click", async (e) => {
   const html = `<div>
   <h4>Word : ${data[0].word}</h4>
   <p>Part of speech : ${data[0].meanings[0].partOfSpeech}</p>
-  <p>Synosnyms : ${data[0].meanings[0].definitions[0].definition}</p>
+  <p>Definition : ${data[0].meanings[0].definitions[0].definition}</p>
   </div>`;
 
   document.querySelector(".display").innerHTML = html;
